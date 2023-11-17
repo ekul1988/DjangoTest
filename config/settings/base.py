23e35e7b489c819a3367c58d96d9913ecd8bda24
Luke <51940865+ekul1988@users.noreply.github.com>
2023-11-17 17:43:28 -0500

    billing app

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "dtp.users",
+    "dtp.billing",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
